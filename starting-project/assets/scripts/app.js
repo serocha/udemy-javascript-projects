7ee0cdf7ef9447d5edd37935bc7c8435c4fb1371
Shane Rocha <67454127+serocha@users.noreply.github.com>
2021-03-15 13:43:27 -0600

    update

--- starting-project/assets/scripts/app.js
+++ starting-project/assets/scripts/app.js
@@ -1,21 +1,75 @@
-const defaultTotal = 0;
-let currentTotal = defaultTotal;
+/* 
+    A short project to run a funky calculator app for a Udemy course.
+    Takes a number input, converts to integer, performs a simple math operation.
+    Also showcases the last operation as a string.
+*/
+
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+//Takes the number string from the userInput field
+const getUserNumberInput = () => {
+    return parseInt(userInput.value);
+}
+
+//Save operations to logEntries
+const writeToLog = (logEntry) => {
+
+    switch (logEntry.operation) {
+        case '+': logEntry.operation = 'ADD';
+        case '-': logEntry.operation = 'SUBTRACT';
+        case '*': logEntry.operation = 'MULTIPLY';
+        case '/': logEntry.operation = 'DIVIDE';
+    }
+
+    logEntries.push(logEntry);  
+}
+
+//Prints string of current operation
+const writeOutput = ( operator, initialResult, enteredNumber) => {
+    const calcDescription = `${initialResult} ${operator} ${enteredNumber}`;
+    outputResult(currentResult, calcDescription); //function from vendor field
+    
+    const logEntry = {
+        operation: operator,
+        prevResult: initialResult,
+        number: enteredNumber,
+        result: currentResult
+    };
+    writeToLog(logEntry);
+}
 
 const add = () => {
-    currentTotal += userInput.value;
-    outputResult(currentTotal, '')
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    writeOutput('+', initialResult, enteredNumber);
 }
 
 const subtract = () => {
-    currentTotal -= num;
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    writeOutput('-', initialResult, enteredNumber);
 }
 
-const multiply = (num) => {
-    currentTotal *= num;
+const multiply = () => {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    writeOutput('*', initialResult, enteredNumber);
 }
 
-const divide = (num) => {
-    currentTotal /= num;
+const divide = () => {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    writeOutput('/', initialResult, enteredNumber);
 }
 
-addBtn.addEventListener( 'click', add );
+//buttons are defined in vendor.js
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
